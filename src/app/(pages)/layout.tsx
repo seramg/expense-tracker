e2846aa253d90073a0dfe2ecb7bd2553f9d3669f
seramg <leelasera19@gmail.com>
2025-11-09 20:19:42 +0530

    feat(folder structure): Refactor all pages into a group (pages)

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import '../globals.css';
 import ReactQueryProvider from '@/components/providers/ReactQueryProvider';
 import { Toaster } from '@/components/ui/sonner';
 
